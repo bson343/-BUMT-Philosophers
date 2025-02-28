a04d92dedfde764ca98a0ee5cc1b6f8fe96020fa
bson_HPLAPTOP <bson7596@gmail.com>
2025-02-28 21:35:18 +0900

    프로젝트 폴더 구조 변경
    refactor: root폴더에 bin, obj, src 형태로 구조 개선
    refactor: sln 파일을 project 폴더로 이동하여 폴더 구조 개선
    refactor: 추가 될 프로젝트들은 모든 src폴더 하위에 추가 될 예정
    BREAKING CHANGE: 솔루션 파일 위치 변경으로 기존의 경로 참조가 달라졌습니다.

--- src/Philosophers/main.cpp
+++ src/Philosophers/main.cpp
@@ -5,8 +5,6 @@
 
 using namespace std;
 
-
-
 struct Arguments
 {
     int numberOfPhilo = -1;
@@ -18,17 +16,19 @@ struct Arguments
 
 void printArguments(Arguments& param)
 {
-    cout << "Show Input Arguments" << endl;
+    cout << "*******Show Input Arguments()************" << endl;
     cout << "numberOfPhilo : " << param.numberOfPhilo << endl;
     cout << "timeToDie : " << param.timeToDie << endl;
     cout << "timeToEat : " << param.timeToEat << endl;
     cout << "timeToSleep : " << param.timeToSleep << endl;
     cout << "timesMustEat : " << param.timesMustEat << endl;
+    cout << "*****************************************" << endl;
 }
 
 int main(int argc, char** argv)
 {
     cout << "Project Philosophers\n";
+    cout << "Console.Ver\n";
 
     cout << "argc : " << argc << endl << endl;
 
@@ -51,38 +51,30 @@ int main(int argc, char** argv)
             &initData.timeToSleep,
             &initData.timesMustEat,
         };
-
-        cout << "Check Arguments" << endl;
-        for (int i = 0; i < argc; i++)
+#pragma region Check Arguments
         {
-            cout << i << " : " << argv[i] << endl;
-
-            int temp = atoi(argv[i]);
-            if (temp < 0)
+            cout << "Check Arguments" << endl;
+            for (int i = 0; i < argc; i++)
             {
-                cerr << "\033[0;31m" << "Invaild Arguments" << "\033[0m" << endl;
-                cout << "The Arguments must be positive." << endl;
-                exit(1);
-            }
+                //cout << i << " : " << argv[i] << endl;
 
-            if (i != 0)
-            {
-                *mapping[i - 1] = temp;
+                int temp = atoi(argv[i]);
+                if (temp < 0)
+                {
+                    cerr << "\033[0;31m" << "Invaild Arguments" << "\033[0m" << endl;
+                    cout << "The Arguments must be positive." << endl;
+                    exit(1);
+                }
+
+                if (i != 0)
+                {
+                    *mapping[i - 1] = temp;
+                }
             }
         }
+#pragma endregion
 
         printArguments(initData);
     }
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
